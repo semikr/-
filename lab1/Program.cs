9c70461d4cfd07471c59dc863850ce8e86c89df0
semikr <rybkinsemen50@gmail.com>
2024-05-18 20:36:53 +0700

    2

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -7,26 +8,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        var tanks = GetTanks();
-        var units = GetUnits();
-        var factories = GetFactories();
+        //var tanks = GetTanks();
+        //var units = GetUnits();
+        //var factories = GetFactories();
 
-        for (int i = 0; i < tanks.Length; i++) 
+        string TankPath = "Tank.json";
+        string UnitPath = "Unit.json";
+        string FactoryPath = "Factory.json";
+
+        string DejsonString1 = File.ReadAllText(TankPath);
+        List<Tank> tanks = JsonSerializer.Deserialize<List<Tank>>(DejsonString1)!;
+
+        string DejsonString2 = File.ReadAllText(UnitPath);
+        List<Unit> units = JsonSerializer.Deserialize<List<Unit>>(DejsonString2)!;
+
+        string DejsonString3 = File.ReadAllText(FactoryPath);
+        List<Factory> factories = JsonSerializer.Deserialize<List<Factory>>(DejsonString3)!;
+
+
+
+        for (int i = 0; i < tanks.Count; i++) 
         { 
-            var u = FindUnit(units, tanks, tanks[i].Name);
-            var f = FindFactory(factories, u);
-            Console.WriteLine($"Tank: {tanks[i].Name}, Unit: {u.Name}, Factory {f.Name}");
+            //var u = FindUnit(units, tanks, tanks[i].Name);
+            var u2 = FindUnit2(units, tanks, tanks[i].Name);
+            //var f = FindFactory(factories, u);
+            var f2 = FindFactory2(factories, u2);
+            //Console.WriteLine($"Tank: {tanks[i].Name}, Unit: {u.Name}, Factory {f.Name}");
+            Console.WriteLine($"Tank: {tanks[i].Name}, Unit: {u2.Name}, Factory {f2.Name}");
         }
 
        
-        Console.WriteLine($"Количество резервуаров: {tanks.Length}, установок: {units.Length}");
+        Console.WriteLine($"Количество резервуаров: {tanks.Count}, установок: {units.Count}");
 
-        var foundUnit = FindUnit(units, tanks, "reservoir 2");
-        var factory = FindFactory(factories, foundUnit);
+        var foundUnit = FindUnit2(units, tanks, "reservoir 2");
+        var factory = FindFactory2(factories, foundUnit);
 
         Console.WriteLine($"Резервуар 2 принадлежит установке {foundUnit.Name} и заводу {factory.Name}");
 
-        var totalVolume = GetTotalVolume(tanks);
+        var totalVolume = GetTotalVolume2(tanks);
         Console.WriteLine($"Общий объем резервуаров: {totalVolume}");
 
 
@@ -34,15 +53,24 @@ class Program
         // Поиск резервуара по имени
         Console.Write("Enter the name of the tank to search: ");
         string searchName = Console.ReadLine();
-        var un = FindUnit(units, tanks, searchName);
+        var un = FindUnit2(units, tanks, searchName);
         if(un != null) { 
-        var fa = FindFactory(factories, un);
+        var fa = FindFactory2(factories, un);
         Console.WriteLine($"Tank: {searchName}, Unit: {un.Name}, Factory {fa.Name}");
         }
         else
         {
             return;
         }
+
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString1 = JsonSerializer.Serialize(GetTanks(), options);
+        string jsonString2 = JsonSerializer.Serialize(GetUnits(), options);
+        string jsonString3 = JsonSerializer.Serialize(GetFactories(), options);
+
+        Console.WriteLine(jsonString1);
+        Console.WriteLine(jsonString2);
+        Console.WriteLine(jsonString3);
 
     }
 
@@ -70,11 +98,11 @@ class Program
     
     // реализуйте этот метод, чтобы он возвращал массив резервуаров, согласно приложенным таблицам
     // можно использовать создание объектов прямо в C# коде через new, или читать из файла (на своё усмотрение)
-    public static Tank[] GetTanks()
+    public static List<Tank> GetTanks()
     {
-        Tank[] tanks =
+        List<Tank> tanks = new List<Tank>()
             {
-                new Tank(1, "reservoir 1", "aboveground - vertical", 1500, 2000, 1),
+                new Tank (1, "reservoir 1", "aboveground - vertical", 1500, 2000, 1),
                 new Tank(2, "reservoir 2", "aboveground - horizontal", 2500, 3000, 1),
                 new Tank(3, "additional reservoir 24", "aboveground - horizontal", 3000, 3000, 2),
                 new Tank(4, "reservoir 35", "aboveground - vertical", 3000, 3000, 2),   
@@ -85,9 +113,9 @@ class Program
  
     }
     // реализуйте этот метод, чтобы он возвращал массив установок, согласно приложенным таблицам
-    public static Unit[] GetUnits()
+    public static List<Unit> GetUnits()
     {
-        Unit[] units =
+        List<Unit> units = new List<Unit>()
             {
             new Unit(1, "GFU-2", "Gas fractionating unit", 1),
             new Unit(2, "AVT-6", "Atmospheric vacuum tube", 1),
@@ -96,9 +124,9 @@ class Program
         return units;
     }
     // реализуйте этот метод, чтобы он возвращал массив заводов, согласно приложенным таблицам
-    public static Factory[] GetFactories()
+    public static List<Factory> GetFactories()
     {
-        Factory[] factories =
+        List<Factory> factories = new List<Factory>()
             {
                 new Factory(1, "NPZ№1", "First NPZ"),
                 new Factory(2, "NPZ№2", "Second NPZ")
@@ -110,50 +138,93 @@ class Program
     // реализуйте этот метод, чтобы он возвращал установку (Unit), которой
     // принадлежит резервуар (Tank), найденный в массиве резервуаров по имени
     // учтите, что по заданному имени может быть не найден резервуар
-    public static Unit FindUnit(Unit[] units, Tank[] tanks, string tankName)
+    //public static Unit FindUnit(List<Unit> units, List<Tank> tanks, string tankName)
+    //{
+    //    for (int i = 0; i < tanks.Count; i++)
+    //    {
+    //        if (tanks[i].Name == tankName)
+    //        {
+    //            return units[tanks[i].UnitId-1];
+    //        }
+    //        else if (tanks[i].Name != tankName & i == 5)
+    //        {
+    //            Console.WriteLine("По данному имени не был найден резервуар");
+    //        }
+    //    }
+    //    return null;
+        
+    //}
+
+    public static Unit FindUnit2(List<Unit> units, List<Tank> tanks, string tankName)
     {
-        for (int i = 0; i < tanks.Length; i++)
+        Tank selectedTank = (from t in tanks where t.Name == tankName select t).FirstOrDefault();
+
+        if (selectedTank != null)
         {
-            if (tanks[i].Name == tankName)
-            {
-                return units[tanks[i].UnitId-1];
-            }
-            else if (tanks[i].Name != tankName & i == 5)
-            {
-                Console.WriteLine("По данному имени не был найден резервуар");
-            }
+            return (from u in units where u.Id == selectedTank.UnitId select u).FirstOrDefault();
         }
-        return null;
+        else
+        {
+            Console.WriteLine("По данному имени не был найдер резервуар");
+            return null;
+        }
+
         
     }
-  
+
     // реализуйте этот метод, чтобы он возвращал объект завода, соответствующий установке
-    public static Factory FindFactory(Factory[] factories, Unit unit)
+    //public static Factory FindFactory(List<Factory> factories, Unit unit)
+    //{
+    //    for (int i = 0; i < factories.Count; i++)
+    //    {
+    //        if (factories[i].Id == unit.FactoryId)
+    //        {
+    //            return factories[i];
+    //        }
+    //        else if(factories[i].Id != unit.FactoryId & i == 1)
+    //            { Console.WriteLine("По данному имени не была найдена фабрика"); }
+    //    }
+    //    return null; 
+    //}
+
+    public static Factory FindFactory2(List<Factory> factories, Unit unit)
     {
-        for (int i = 0; i < factories.Length; i++)
+        Factory selectedFactory = (from f in factories where f.Id == unit.FactoryId select f).FirstOrDefault();
+
+        if(selectedFactory != null)
         {
-            if (factories[i].Id == unit.FactoryId)
-            {
-                return factories[i];
-            }
-            else if(factories[i].Id != unit.FactoryId & i == 1)
-                { Console.WriteLine("По данному имени не была найдена фабрика"); }
+            return selectedFactory;
         }
-        return null; 
+        else
+        {
+            Console.WriteLine("По данному имени не была найдена фабрика");
+            return null;
+        }
     }
 
     // реализуйте этот метод, чтобы он возвращал суммарный объем резервуаров в массиве
-    public static int GetTotalVolume(Tank[] units)
-    {
-        int totalVolume = 0;
+    //public static int GetTotalVolume(List<Tank> tanks)
+    //{
+    //    int totalVolume = 0;
 
-        for(int i = 0; i < units.Length; i++)
-        {
-            totalVolume = totalVolume + units[i].Volume;    
-        }
+    //    for(int i = 0; i < units.Count; i++)
+    //    {
+    //        totalVolume = totalVolume + units[i].Volume;    
+    //    }
+    //    return totalVolume;
+    //}
+
+    public static int GetTotalVolume2(List<Tank> tanks)
+    {
+        int totalVolume = tanks.Sum(t => t.Volume);
         return totalVolume;
     }
+
+
 }
+
+
+
 
 
 // Установка
@@ -212,5 +283,9 @@ public class Tank
         MaxVolume = maxVolume;
         UnitId = unitId;
     }
+
+    
 }
+
+
 
---
+++ lab2part2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "JSON_sample_1.json";
+
+        var deals = GetDeals(path);
+        
+        Console.WriteLine("Сделки: ");
+        for (int i = 0; i < deals.Count(); i++)
+        {
+            Console.WriteLine($"Sum: {deals.ElementAt(i).Sum}\nId: {deals.ElementAt(i).Id}\nDate: {deals.ElementAt(i).Date}\n");
+        }
+
+        Console.WriteLine("Отфильтрованные сделки: ");
+
+        var selectedDeals = GetNumbersOfDeals(deals);
+        foreach (string i in selectedDeals)
+        {
+            Console.WriteLine(i);
+        }
+
+        var sumsByMonth = GetSumsByMonth(deals);
+
+        foreach (var sumByMonth in sumsByMonth)
+        {
+            Console.WriteLine($"Month: {sumByMonth.Month.ToString("yyyy-MM")}, Sum: {sumByMonth.Sum}");
+        }
+    }
+
+    public static IEnumerable<Deal> GetDeals(string path)
+    {
+        string jsonString = File.ReadAllText(path);
+        IList<Deal> deals = JsonSerializer.Deserialize<IList<Deal>>(jsonString)!;   
+        return deals;
+    }
+
+    public static IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
+    {
+        return deals.Where(d => d.Sum >= 100).OrderBy(d => d.Date).Take(5).OrderByDescending(d => d.Sum).Select(d => d.Id).ToList();
+    }
+
+    public record SumByMonth(DateTime Month, int Sum);
+
+    public static IList<SumByMonth> GetSumsByMonth(IEnumerable<Deal> deals)
+    {
+        return deals.GroupBy(d => new { Month = new DateTime(d.Date.Year, d.Date.Month, 1) }).Select(dls => new SumByMonth(dls.Key.Month, dls.Sum(d => d.Sum))).ToList();
+    }
+
+
+    public class Deal
+    {
+        public int Sum { get; set; }
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+   
+}
